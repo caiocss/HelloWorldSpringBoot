11bf1194ea6d9358eca883fac4f76197d475e7ac
Caio Santos <caio.santos687@gmail.com>
2019-09-10 23:35:32 -0300

    init project

---
+++ src/main/java/com/example/demoapiproject/DemoapiprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoapiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoapiprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoapiprojectApplication.class, args);
+    }
+
+}
